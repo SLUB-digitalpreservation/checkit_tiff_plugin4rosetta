939c431539fca8d3947f69ccd78a5c509071501e
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2016-05-30 17:14:38 +0200

    - fixed package

--- java/org/slub/rosetta/dps/repository/plugin/SLUBTechnicalMetadataExtractorCheckItTiffPlugin.java
+++ java/org/slub/rosetta/dps/repository/plugin/SLUBTechnicalMetadataExtractorCheckItTiffPlugin.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package org.slub.rosetta.dps.repository.plugin.mdExtractor;
+package org.slub.rosetta.dps.repository.plugin;
 
 
 import com.exlibris.dps.sdk.techmd.MDExtractorPlugin;
